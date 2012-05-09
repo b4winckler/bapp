d112cbe2784a61bffc44edb646b7803a19e68252
Bjorn Winckler <bjorn.winckler@gmail.com>
2012-05-09 21:58:02 +0200

    Initial revision

---
+++ R/bapp.R
@@ -0,0 +1,101 @@
+# Plot binned data.
+#
+# The parameter 'x' is the bin labels (i.e. categories, factors) and 'y' is a
+# vector of the same length as 'x'.  Both variable names may be columns in the
+# data frame 'data' (if specified).  A line connecting the median of each bin
+# is drawn in color 'medianColor'.  The interquartile range of the bins are
+# shaded in color 'quartileColor' (don't shade if this is NULL).  Finally, the
+# range between the min and max of each bin is shaded in color 'rangeColor'
+# (don't shade if this is NULL).
+#
+# Examples:
+#
+# plotBins(rep(1:10,10), rnorm(100))
+#
+# Example on how to plot male plasma samples for antibody 1901 and sort them
+# into 5 year bins (this assumes df$age, df$gender, df$type, and df$Ab_1891
+# exist):
+#
+#   df <- loadAGE()
+#   plotBins(bins(age,width=5), Ab_1891,
+#            data=subset(df, gender == 'male' & type == 'plasma'),
+#            main="A protein profile", xlab="Age" ylim=c(185,235))
+#
+plotBins <- function(x, y,
+                     data = data.frame(),
+                     medianColor = 'blue',
+                     quartileColor = rgb(0.5,0.5,0.5,0.3),
+                     rangeColor = rgb(0.5,0.5,0.5,0.1),
+                     xlab = deparse(substitute(x)),
+                     ylab = deparse(substitute(y)), ...)
+{
+    # Evaluate the 'x' and 'y' arguments inside the database 'data' so that
+    # they can refer to variables inside 'data'.
+    attach(data)
+    X <- x
+    Y <- y
+    detach()
+
+    # Scatter plot data points ('plot' will do box-and-whisker plot if 'X' is a
+    # factor, hence the use of 'plot.default').
+    plot.default(X, Y, xlab=xlab, ylab=ylab, ...)
+
+    # Determine quantiles
+    qs <- tapply(Y, X, quantile, na.rm=TRUE)
+    # Figure out which x coordinates to use
+    qx <- if (is.factor(X)) 1:length(qs) else names(qs)
+    # Convert quantiles into matrix whose rows are: min,q1,median,q3,max
+    qm <- matrix(unlist(qs), nrow=5)
+
+    # Draw shaded area between min and max
+    if (!is.null(rangeColor))
+        polygon(c(qx,rev(qx)), c(qm[1,],rev(qm[5,])), col=rangeColor)
+
+    # Draw shaded area between 1st and 3rd quartiles
+    if (!is.null(quartileColor))
+        polygon(c(qx,rev(qx)), c(qm[2,],rev(qm[4,])), col=quartileColor)
+
+    # Draw line on median
+    lines(qx, qm[3,], col=medianColor, lw=3)
+}
+
+bins <- function(x, n=5)
+{
+    bs <- seq(from=n/2, to=max(x)+n, by=n)
+    ls <- seq(from=n, by=n, length.out=length(bs)-1)
+    a  <- cut(x, breaks=bs, labels=ls)
+    as.numeric(levels(a))[a]
+}
+
+randomProfile <- function(binCount, binSize)
+{
+    data.frame(x=rep(1:binCount, binSize), y=rnorm(binCount*binSize))
+}
+
+# Compute signature scores.  The parameter 'cats' contains the bin labels, and
+# 'pts' is a vector of the same length as 'cats'.
+#
+# NOTE! This function requires the 'sigscore' tool to be in the $PATH.
+#
+sigscore <- function(cats, pts)
+{
+    if (is.vector(pts) && length(cats) == length(pts))
+        pts <- as.matrix(pts)
+
+    if (length(cats) != dim(pts)[1]) {
+        warning('Number of categories does not match rows of data')
+        return()
+    }
+
+    m <- rbind(cats, t(pts))
+
+    message('Writing temporary CSV file...')
+    fname <- tempfile()
+    csv <- write.table(m, file=fname, row.names=FALSE, col.names=FALSE,
+                       na="", sep=",")
+
+    message('Computing signature scores...')
+    z <- system(paste('sigscore',fname), intern=TRUE)
+    system(paste('rm',fname))
+    as.numeric(z)
+}
---
+++ R/peaks-mixture-model.R
@@ -0,0 +1,123 @@
+library(mclust)
+library(ggplot2)
+
+
+# Split well into groups by bead ID, then apply 'FUN' to each group in turn.
+applyWell <- function(well, FUN=identity)
+{
+    tapply(well[ ,'RP1'], well[ ,'RID'], FUN)
+}
+
+# Print summary of peaks and median for the given well.  Call
+#
+#   lapply(wells, peakSummary)
+#
+# to print a summary for a list of wells.
+peakSummary <- function(well)
+{
+    data.frame(best=applyWell(well,FUN=bestPeak),
+               left=applyWell(well,FUN=leftPeak),
+               median=applyWell(well,FUN=median),
+               right=applyWell(well,FUN=rightPeak))
+}
+
+# Plot each bead density in well and mark left&right peaks as well as the
+# median.
+plotPeaks <- function(well)
+{
+    devAskNewPage(TRUE)
+    applyWell(well, plotPeak)
+    devAskNewPage(FALSE)
+}
+
+# Plot density for 'xlin' and mark the left&right peaks as well as the median.
+plotPeak <- function(xlin)
+{
+    p  <- suppressWarnings(estimatePeaks(xlin))
+    df <- as.data.frame(p)
+    print(df)
+    if (any(is.na(df))) {
+        warning('Peak estimation failed, skipping...')
+        return()
+    }
+
+    # Detect which peak to pick:
+    # Take the one with smallest variability, unless it has a proportion
+    # smaller than 'eps'.
+    eps <- 0.25
+    l <- p$var[1] < p$var[2]
+    if (l && p$prop[1] < eps)  l <- FALSE
+    if (!l && p$prop[2] < eps) l <- TRUE
+
+    hist <- qplot(xlin, geom="density", log="x")
+    print(hist +
+          geom_histogram(aes(y = ..density..), alpha = 0.1) +
+          geom_vline(aes(xintercept=p$mean[1]), col="red",
+                     linetype=ifelse(l, "solid", "dashed")) +
+          geom_vline(aes(xintercept=p$mean[2]), col="blue",
+                     linetype=ifelse(l, "dashed", "solid")) +
+          geom_vline(aes(xintercept=median(xlin)), col="green",
+                     linetype="dashed"))
+}
+
+# Use two-Gaussian mixture model to detect the peaks (i.e. means of the two
+# Gaussians) of 'xlin'.  The data 'xlin' is log-transformed before applying the
+# mixture model.  Hence for the estimate to be somewhat accurate the data
+# should have two near-Gaussian bumps after being log-transformed.
+#
+# The return value is a list which describes the proportion, mean, and variance
+# of the fitted Gaussians.
+estimatePeaks <- function(xlin)
+{
+    # The data is assumed to be linear.  Take logarithm first so that the data
+    # is closer to Gaussian.
+    x <- log(xlin[!is.na(xlin) & xlin>0])
+
+    # Need to supply initial guess as to which group each sample belongs to.
+    # Our guess is:
+    #   - Group 1, if sample <= median
+    #   - Group 2, if sample >  median
+    guess <- unmap(x > median(x))
+
+    # Perform EM algorithm to estimate parameters
+    #   "E" - equal variance
+    #   "V" - variable variance
+    ms <- me("V", x, guess)
+    es <- em("V", x, ms$parameters)
+
+    # Mean is converted back to linear.
+    par <- es$parameters
+    list(prop=par$pro, mean=exp(par$mean), var=par$variance$sigmasq)
+}
+
+# Pick out left peak from two-Gaussian mixture model.
+leftPeak <- function(xlin)
+{
+    p <- suppressWarnings(estimatePeaks(xlin))
+    p$mean[1]
+}
+
+# Pick out right peak from two-Gaussian mixture model.
+rightPeak <- function(xlin)
+{
+    p <- suppressWarnings(estimatePeaks(xlin))
+    p$mean[2]
+}
+
+# Pick out "best" peak from two-Gaussian mixture model.
+bestPeak <- function(xlin, eps=0.25)
+{
+    p  <- suppressWarnings(estimatePeaks(xlin))
+    df <- as.data.frame(p)
+    if (any(is.na(df)))
+        return(NA)
+
+    # Detect which peak to pick:
+    # Take the one with smallest variability, unless it has a proportion
+    # smaller than 'eps'.
+    l <- p$var[1] < p$var[2]
+    if (l && p$prop[1] < eps)  l <- FALSE
+    if (!l && p$prop[2] < eps) l <- TRUE
+
+    if (l) p$mean[1] else p$mean[2]
+}
