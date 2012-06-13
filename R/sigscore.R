121ab603e7978b911f229fb8881a6d9b120a04ec
Bjorn Winckler <bjorn.winckler@gmail.com>
2012-06-13 13:10:26 +0200

    Add option to make 'sigscore()' quiet

--- R/sigscore.R
+++ R/sigscore.R
@@ -1,9 +1,14 @@
-# Compute signature scores.  The parameter 'cats' contains the bin labels, and
-# 'pts' is a vector of the same length as 'cats'.
+# Compute signature scores.  The parameter 'cats' is a vector of bin labels,
+# and 'pts' is a matrix with as many rows as there are elements in 'cats'.
+#
+# Computing the signature score can take a long time and for this reason a
+# progress bar is displayed during the computation.  Set 'quiet=TRUE' to
+# suppress the progress bar.  Use fewer categories to speed up the computation
+# (the 'bins()' function can be used for this).
 #
 # NOTE! This function requires the 'sigscore' tool to be in the $PATH.
 #
-sigscore <- function(cats, pts)
+sigscore <- function(cats, pts, quiet=FALSE)
 {
     if (is.vector(pts))
         pts <- as.matrix(pts)
@@ -13,12 +18,13 @@ sigscore <- function(cats, pts)
 
     m <- rbind(cats, t(pts))
 
-    message('Computing signature scores...')
+    if (!quiet)
+        message('Computing signature scores...')
     fname <- tempfile()
     csv <- write.table(m, file=fname, row.names=FALSE, col.names=FALSE,
                        na="", sep=",")
 
-    z <- system(paste('sigscore',fname), intern=TRUE)
+    z <- system(paste('sigscore',fname), intern=TRUE, ignore.stderr=quiet)
     system(paste('rm',fname))
     as.numeric(z)
 }
